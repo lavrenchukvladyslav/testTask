89d5288d4bf0df6327e021f865e4d760c40e95fc
lavrenchukvladyslav <32793860+lavrenchukvladyslav@users.noreply.github.com>
2018-04-25 17:15:21 +0300

    add test task

---
+++ Printe/script.js
@@ -0,0 +1,12 @@
+$(".phones").hide();
+$(function () {
+    $(".dropdown").click(function () {
+        $(".phones").slideToggle(500);
+    });
+});
+$(".phones2").hide();
+$(function () {
+    $(".dropdown2").click(function () {
+        $(".phones2").slideToggle(500);
+    });
+});
